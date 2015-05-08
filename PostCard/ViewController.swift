13403a949d0b8694f677806731796b6aea28a86d
jpjananraja <jpjananraja@gmail.com>
2015-05-08 19:19:06 +0530

    First rev of Project

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var sendMailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+//    IBAction methods
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton)
+    {
+        messageLabel.hidden = false //unhides the button
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        
+        
+        enterMessageTextField.text = ""
+        
+        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+        
+        
+        
+        sendMailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
+        
+        
+        
+        
+    
+    
+    }
 
 }
 
