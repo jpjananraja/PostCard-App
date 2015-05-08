32d7bdd6687ebecc49a2996ef253375a996ab226
jpjananraja <jpjananraja@gmail.com>
2015-05-08 19:52:42 +0530

    Name label added commits

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var sendMailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +38,12 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
-        
+        nameLabel.hidden  = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
         
         enterMessageTextField.text = ""
+        enterNameTextField.text = ""
         
         enterMessageTextField.resignFirstResponder()
         enterNameTextField.resignFirstResponder()
